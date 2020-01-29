2f7326f5c170ac87279e8846feaa72b6447cdb12
VIGNESH <60211429+vignesh336@users.noreply.github.com>
2020-01-29 14:12:31 +0530

    Create hangman_game.py

---
+++ hangman_game.py
@@ -0,0 +1,69 @@
+import random 
+ 
+  
+name = input("What is your name? ") 
+ 
+  
+print("Good Luck ! ", name) 
+  
+words = ['iteminam','sunnisamson','pottaimmanuel','oolubhuvanesh','dandanakadinesh']  
+  
+
+word = random.choice(words) 
+  
+  
+print("Guess the characters") 
+  
+guesses = '' 
+  
+ 
+turns = 12
+  
+  
+while turns > 0: 
+      
+     
+    failed = 0
+      
+
+    for char in word:  
+          
+ 
+        if char in guesses:  
+            print(char) 
+              
+        else:  
+            print("_") 
+              
+           
+            failed += 1
+              
+  
+    if failed == 0: 
+      
+        print("You Win")  
+          
+       
+        print("The word is: ", word)  
+        break
+      
+
+    guess = input("guess a character:") 
+      
+    
+    guesses += guess  
+      
+   
+    if guess not in word: 
+          
+        turns -= 1
+          
+          
+        print("Wrong") 
+          
+        
+        print("You have", + turns, 'more guesses') 
+          
+          
+        if turns == 0: 
+            print("You Loose") 
